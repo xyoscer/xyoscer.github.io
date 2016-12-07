a112a67209db49340148a0f6eeae4c20dc68c794
xyoscer <1498546245@qq.com>
2016-12-07 20:39:06 +0800

    summary

--- ife/task18/task18.js
+++ ife/task18/task18.js
@@ -8,104 +8,102 @@ function addEventHandler(ele, event, hanlder) {
         ele["on" + event] = hanlder;
     }
 }
-//实现对列的类
-function Queue(){
-	var items = [];
+var items = [];//存放队列元素中的数组
+var queue = {	 
 
-	this.Lenqueue = function(element){
-		items.unshift(element);
-	};
-
-	this.Renqueue = function(element){
-		items.push(element);
-	};
-
-	this.Ldequeue = function(){
-		items.shift();
-       
-	};
-	
-	this.Rdequeue = function(){
-		items.pop();
-       
-	};	
-	this.size = function(){
-		return items.length;
-	};
-	this.print = function(){
-		return items;
-	}
+	//对列左近
+	Lenqueue: function(ele) {
+         items.unshift(ele);
+	},
+	//对列右近
+	Renqueue: function(ele) {
+		items.push(ele);
+	},
+	Ldequeue: function(){
+		return(items.shift());       
+	},
+	Rdequeue: function(){
+		return(items.pop());       
+	},	
+   
 }
 
-var leftRu = document.getElementById('leftRu');
-var rightRu = document.getElementById('rightRu');
 var queueDiv = document.getElementById('queue');
-var leftChu = document.getElementById('leftChu');
-var rightChu = document.getElementById('rightChu'); 
+var parentBtn = document.getElementById('parentBtn');
+ addEventHandler(parentBtn,'click',move);
+ function move(event) {
+    var e = event||window.event;
+    var target = event.target||event.srcElement;
+    
+    switch(target.id) {
+    	case 'leftRu':
+    	      var inputValue = getInputValue();
+    	       if(inputValue) {
+	 	        queue.Lenqueue(inputValue);	 	       
+	          }
+    	  break;
+    	case  'rightRu':
+    	      var inputValue = getInputValue();
+    	      if(inputValue) {
+	 	         queue.Renqueue(inputValue);	
 
-    addEventHandler(leftRu,'click', enqueueleftRu);
-    addEventHandler(rightRu,'click', enqueuerightRu);
-    addEventHandler(leftChu,'click', enqueueleftChu);  
-    addEventHandler(rightChu,'click', enqueuerightChu);
+	          }
+    	  break;
+    	case  'leftChu':    	     
+	 	        var itemValue = queue.Ldequeue(inputValue); 
+	 	        alert("删除的元素是"+itemValue);        
+	          
+    	 break;
+    	 case 'rightChu':    	   
+	 	        var ritemValue = queue.Rdequeue(inputValue);
+	 	        alert("删除的元素是"+ritemValue);	 	        
+	         
+    	 break;
 
-var queue = new Queue();
+     }
+      viewData(items);
+ }
+   
+
 
 //获得input中的值
 function getInputValue(){
 	var input = document.getElementById('num');
 	var inputValue = input.value.trim();
-	if(inputValue.match(/^[0-9]+$/)){
+	if(inputValue.match(/^[0-9]+$/)) {
 		   return inputValue;
 		}
-		else{
+	else {
 			alert("请输入整数！");
 		}
 }
 //可视化队列数据
 function viewData(showArr){
-	var text="",color="";
-     for(var i=0;i<showArr.length;i++){		
-	   	color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-		text+="<div id='queueItem' style = 'background-color:"+color+"'>"+showArr[i]+"</div>";
+	var text = "";
+     for(var i=0 ,len=showArr.length;i<len;i++){	    
+		/*text += '<div class="queueItem"'
+		     +'data-item="'
+		     +i
+		     +'">'
+		     +showArr[i]
+		     +'</div>';*/
+		    //尝试使用ES6的模板字符串来添加，表示很方便
+		  text += `<div class="queueItem" data-item=${i}>${showArr[i]}</div>`
 	}
-	queueDiv.innerHTML="";
-	queueDiv.innerHTML+=text;   
-	document.getElementById('num').value="";
+	queueDiv.innerHTML = "";
+	queueDiv.innerHTML += text;   
+	document.getElementById('num').value = "";
+	var queueItems = document.getElementsByClassName('queueItem');
+	//为每一个新增的元素添加点击删除元素，使用splice(),删除完元素，在进行重新显示数组
+    [].forEach.call(queueItems,function(v){
+   	   addEventHandler(v, "click", function() {
+   	   	  items.splice(v.dataset.item, 1);
+   	   	  viewData(items);
+   	   });
+   	  
+   })
+
 }
 
-//左入
-function enqueueleftRu(){	
-	var inputValue = getInputValue();	
-		queue.Lenqueue(inputValue);
-	var showArr = queue.print();
-	    viewData(showArr);	
-
-} 
-//右入
-function enqueuerightRu(){	
-	
-	var inputValue = getInputValue();
-		queue.Renqueue(inputValue);
-	var showArr = queue.print();
-	    viewData(showArr);	
-	  
-	
-} 
-//左出
-function enqueueleftChu(){	
-	
-	   queue.Ldequeue();
-	var showArr = queue.print();	
-	   viewData(showArr);	
-	
-
-} 
-//右出
-function enqueuerightChu(){	
-
-	queue.Rdequeue();
-	var showArr = queue.print();
-	viewData(showArr);		
-	
-} 
+  
 
